1279d777ea8ed9f88a1326df460744a478092161
KyawThetKhing <ktkhaing47@gmail.com>
2025-03-22 20:46:24 +0700

    feat: firebase setup

---
+++ src/includes/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  appId: import.meta.env.VITE_APP_ID,
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
